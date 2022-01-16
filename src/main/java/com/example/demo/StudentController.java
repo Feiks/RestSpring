77410908d5a483b27d1d8be1c0f2ada57ff30107
felya_97@mail.ru <felya_97@mail.ru>
2022-01-16 16:09:20 +0600

    Added a StudentList arrayList and uset GET method HTTP

--- src/main/java/com/example/demo/StudentController.java
+++ src/main/java/com/example/demo/StudentController.java
@@ -4,12 +4,27 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
 
     @GetMapping("/student")
     public Student getStudent(){
      return  new Student("Feliks","Bekeshov");
+         }
+
+         @GetMapping("/students")
+         public List<Student> studentList(){
+        List<Student> students = new ArrayList<>();
+                     students.add(new Student("Nurlan","Kzakabaev"));
+                     students.add(new Student("Yeha","Kzakabaev"));
+                     students.add(new Student("Murat","Kzakabaev"));
+                     students.add(new Student("Shavkat","Kzakabaev"));
+                     students.add(new Student("Naruh","Kzakabaev"));
+
+             return students;
          }
 
 }
